f63ae844f5bbdb29e5f2a6cce35d6824ab8a37e4
JJBR13 <jjbrown1308@gmail.com>
2022-12-02 09:37:57 +0000

    Import flash, creating message after form has been  sent

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
 import json
 # Capital F indicates its a class name
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -33,7 +36,8 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form.get("name"))
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
@@ -46,5 +50,6 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        # debug should only be True in devel mode, before submission needs to be set to False!
+        # debug should only be True in devel mode,
+        # before submission needs to be set to False!
         debug=True)
